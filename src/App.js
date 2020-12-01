10eba2e8aa80f178dc422239ed36a2146d33db3e
FlorentCmb <AS0D@protonmail.ch>
2020-12-01 11:19:59 +0100

    Add CharacterCard in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+// Components
+import CharacterCard from './components/CharacterCard'
+// Style
+import './App.css'
+
 const App = () => {
+
+  // Render
   return (
     <div className="App">
-      
+      <CharacterCard />
     </div>
   )
 }
